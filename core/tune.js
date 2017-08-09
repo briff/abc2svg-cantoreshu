moinejf/abc2svg

commit 2646b34c6d317190167b4b4acad3fa76bb281d74

core: fix: shift of measure bar when new voice in %%score

core/tune.js
@@ -150,7 +150,7 @@ var w_tb = new Uint8Array([
 
 function sort_all() {
 	var	s, s2, p_voice, v, time, w, wmin, ir, multi,
-		prev, new_sy, nb,
+		prev, nb,
 		nv = voice_tb.length,
 		vtb = [],
 		vn = [],			/* voice indexed by range */
@@ -162,11 +162,13 @@ function sort_all() {
 	/* initialize the voice order */
 	var	fl = 1,
 		sy = cur_sy,
-		set_sy = true
+		sy_w = 0,
+		sy_time = 0,
+		new_sy = 1
 
 	while (1) {
-		if (set_sy && fl) {
-			set_sy = false;
+		if (new_sy && fl) {
+			new_sy = false;
 			multi = -1;
 			vn = []
 			for (v = 0; v < nv; v++) {
@@ -245,6 +247,17 @@ function sort_all() {
 			}
 		}
 
+		// continue the time sequence after STAVES
+		if (sy_w) {
+			if (sy_w < 0) {		// init
+				sy_w = wmin
+			} else if (sy_time == time && sy_w == wmin) {
+				fl = 0
+			} else {
+				sy_w = 0
+			}
+		}
+
 		/* link the vertical sequence */
 		for (ir in vn) {
 			v = vn[ir]
@@ -256,7 +269,9 @@ function sort_all() {
 				continue
 			if (s.type == STAVES) {
 				sy = sy.next;
-				set_sy = true
+				new_sy = true;
+				sy_w = -1;
+				sy_time = s.time
 			}
 			if (fl) {
 				fl = 0;

briff/abc2svg-cantoreshu

commit 40021ded1e4a33443d7b33b113ebebc40db07e62

core: fix: loss of symbols when 2 %%score definitions with no common voice
This bug was introduced by commit 2646b34c6d317190167b4b4acad3fa76bb281d74.
Solve issue #5

FossilOrigin-Name: f5ea4c28d57a688fee3ebf8f31a6cd58f0bfa704

core/tune.js
@@ -210,8 +210,13 @@ function sort_all() {
 			}
 		}
 
-		if (wmin > 127)
-			break					/* done */
+		if (wmin > 127) {
+			if (new_sy && !fl) {	// if %%score with no common voice
+				fl = 1
+				continue
+			}
+			break			// done
+		}
 
 		/* if some multi-rest and many voices, expand */
 		if (time == mrest_time) {