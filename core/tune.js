moinejf/abc2svg

commit 04ee7c60769bac0095671da1be8f9a71c00b5baf

core: editor: util: fix: use or replace 'for in' correctly
More about issue #4

core/tune.js
@@ -30,10 +30,13 @@ function voice_filter() {
 	var opt, sel, tmp, i
 
 	for (opt in parse.voice_opts) {
+		if (!parse.voice_opts.hasOwnProperty(opt))
+			continue
 		sel = new RegExp(opt)
 		if (sel.test(curvoice.id)
 		 || sel.test(curvoice.nm)) {
 			for (i in parse.voice_opts[opt])
+			    if (parse.voice_opts[opt].hasOwnProperty(i))
 				do_pscom(parse.voice_opts[opt][i])
 		}
 	}
@@ -842,6 +845,8 @@ function get_break(param) {
 
 	glovar.break = []
 	for (k in a) {
+		if (!a.hasOwnProperty(k))
+			continue
 		b = a[k];
 		i = b.indexOf(':')
 		if (i < 0) {
@@ -2342,7 +2347,12 @@ function get_voice(parm) {
 // change state from 'tune header after K:' to 'in tune body'
 // curvoice is defined when called from get_voice()
 function goto_tune(is_K) {
-	var s, v, p_voice, transp;
+	var	v, p_voice, transp,
+		s = {
+			type: STAVES,
+			dur: 0,
+			sy: par_sy
+		}
 
 	parse.state = 3;			// in tune body
 
@@ -2359,14 +2369,9 @@ function goto_tune(is_K) {
 	}
 
 	// link the first %%score
-	if (staves_found >= 0) {
-		s = {
-			type: STAVES,
-			dur: 0,
-			sy: par_sy
-		}
-		sym_link(s)
-	}
+	sym_link(s)
+	if (staves_found < 0)
+		s.default = true
 
 	if (!curvoice.init && !is_K) {
 		set_kv_parm([]);

core/abc2svg.js
@@ -103,6 +103,7 @@ function clone(obj) {
 		return obj
 	var tmp = new obj.constructor()
 	for (var k in obj)
+	    if (obj.hasOwnProperty(k))
 		tmp[k] = obj[k]
 	return tmp
 }

core/format.js
@@ -141,6 +141,8 @@ function get_font_scale(param) {
 	}
 	font_scale_tb[a[0]] = scale
 	for (var fn in font_tb) {
+		if (!font_tb.hasOwnProperty(fn))
+			continue
 		var font = font_tb[fn]
 		if (font.name == a[0])
 			font.swfac = font.size * scale

core/front.js
@@ -654,6 +654,7 @@ function tosvg(in_fname,		// file name
 			if (info.V) {
 				info_sav.V = {}
 				for (i in info.V)
+				    if (info.V.hasOwnProperty(i))
 					info_sav.V[i] = clone(info.V[i])
 			}
 			char_tb_sav = clone(char_tb);

core/parse.js
@@ -463,6 +463,7 @@ function set_kv_parm(a) {	// array of items
 	if (pos) {
 		curvoice.pos = clone(curvoice.pos)
 		for (item in pos)
+		    if (pos.hasOwnProperty(item))
 			curvoice.pos[item] = pos[item]
 	}
 
@@ -2282,6 +2283,7 @@ function parse_music_line() {
 
 	// play the macro game
 	for (k in mac) {
+	    if (mac.hasOwnProperty(k))
 		if (k.indexOf('.') >= 0)		// dynamic
 			line.buffer = expand(line.buffer, k)
 		else

core/psvg.js
@@ -253,6 +253,7 @@ function Svg() {
 //		return obj
 	var	k, tmp = new obj.constructor()
 	for (k in obj)
+	    if (obj.hasOwnProperty(k))
 		tmp[k] = obj[k]
 	return tmp
     }

editor/edit.js
@@ -20,7 +20,7 @@
 var	abc_images,			// image buffer
 	abc_fname = ["noname.abc", ""],	// file names
 	abc,				// Abc object
-	ref= [],			// source reference array
+	ref,				// source reference array
 	colcl = [],			// colorized classes
 	abcplay,			// play engine
 	a_pe,				// playing events
@@ -188,7 +188,7 @@ function render() {
 
 	diverr.innerHTML = '';
 //	document.body.style.cursor = "wait";
-	ref = {}
+	ref = []
 	try {
 		abc.tosvg(abc_fname[0], content)
 	} catch(e) {
@@ -362,7 +362,7 @@ var	i, j, elts, d,
 
 // source text selection callback
 function seltxt(elt) {
-	var	i, n, o, start, end, s, z
+	var	start, end, s, z, elts
 	if (colcl.length != 0) {
 		colorsel(false);
 		colcl = []
@@ -374,16 +374,16 @@ function seltxt(elt) {
 	if (start == 0
 	 && end == document.getElementById("source").value.length)
 		return				// select all
-	for (o in ref) {
-		if (o >= start && ref[o] <= end)
+	ref.forEach(function(e, o) {
+		if (o >= start && e <= end)
 			colcl.push('_' + o + '_')
-	}
+	})
 	if (colcl.length != 0) {
 		colorsel(true);
 		s = document.getElementById("dright");
 	  z = window.document.defaultView.getComputedStyle(s).getPropertyValue('z-index')
 		if (z != 10) {			// if select from textarea
-			var elts = document.getElementsByClassName(colcl[0]);
+			elts = document.getElementsByClassName(colcl[0]);
 			elts[0].scrollIntoView()	// move the element on the screen
 		}
 	}

util/json.js
@@ -82,6 +82,7 @@ function AbcJSON(nindent) {			// indentation level
 			} else {
 				h = '{\n'
 				for (i in val)
+				    if (val.hasOwnProperty(i))
 					attr_gen(indn, i, val[i]);
 				json += '\n' + ind + '}'
 			}
@@ -119,6 +120,7 @@ function AbcJSON(nindent) {			// indentation level
 		h += ind2 + '{\n' +
 			ind3 + '"voice_properties": {\n'
 		for (i in voice_tb[v])
+		    if (voice_tb[v].hasOwnProperty(i))
 			attr_gen(ind4, i, voice_tb[v][i]);
 
 		json += '\n' + ind3 + '},\n' +

briff/abc2svg-cantoreshu

commit 1763483da19d0dce2e4866455dd48d97993c1b51

core: fix: crash when voice of 1st V: absent of %%score
Bug introduced by commit 04ee7c60769bac0095671da1be8f9a71c00b5baf and previous.

FossilOrigin-Name: ab7271864254569a929f47880555c272513c97e1

core/tune.js
@@ -2404,8 +2404,11 @@ function goto_tune(is_K) {
 		par_sy.nstaff = nstaff
 	}
 
-	// link the first %%score
+	// link the first %%score in the top voice
+	p_voice = curvoice;
+	curvoice = voice_tb[par_sy.top_voice];
 	sym_link(s)
 	if (staves_found < 0)
-		s.default = true
+		s.default = true;
+	curvoice = p_voice
 }