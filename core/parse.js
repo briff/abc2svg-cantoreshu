e021d26f5a3c801307933cbee6a8c3bf3d59a4d5
jef <jef@noemail.net>
2024-12-26 10:45:19 +0000

    core: play: fix: bad sounding of some accidentals with %%MIDI temperamentequal
    FossilOrigin-Name: 2802a8930a3c870ca5605070b43436fad96d7f84

--- core/parse.js
+++ core/parse.js
@@ -1953,16 +1953,16 @@ function pit2mid(pit, acc) {
 			p += o + s		// standard temperament
 			return p
 		}
-	} else {				// equal temperament
-		if (typeof acc != "object") {	// if not a fraction
-			b40 = abc2svg.p_b40[pit % 7] + acc
-			return cfmt.temper[b40] + o
-		}
-
-		if (acc[1] == cfmt.nedo) { // fraction of the edo divider
-			b40 = abc2svg.p_b40[pit % 7]
-			return cfmt.temper[b40] + o + s
-		}
+//	} else {				// equal temperament
+//		if (typeof acc != "object") {	// if not a fraction
+//			b40 = abc2svg.p_b40[pit % 7] + acc
+//			return cfmt.temper[b40] + o
+//		}
+//
+//		if (acc[1] == cfmt.nedo) { // fraction of the edo divider
+//			b40 = abc2svg.p_b40[pit % 7]
+//			return cfmt.temper[b40] + o + s
+//		}
 	}
 
 	p0 = cfmt.temper[abc2svg.p_b40[pit % 7]]	// main note
