a8e3eb2816d47293718c532109c536df4d7e57a6
jef <jef@noemail.net>
2025-11-09 16:15:37 +0000

    core: fix: better handling of %%maxshrink and %%trimsvg
    FossilOrigin-Name: 0c262a04a1a86e5f71998caa845b1c98d9a92be9

--- core/music.js
+++ core/music.js
@@ -5282,14 +5282,12 @@ Abc.prototype.set_sym_glue = function(width) {
 			if (xx == xse)			// if no space
 				xx += 10
 			spf = (width - xs - xse) / (xx - xse)
-		} else if (cfmt.trimsvg) {
-			spf = 1 - cfmt.maxshrink
 		} else {
 			spf = spf_last
 			if (ll && spf < s.fmt.stretchlast)
 				spf = s.fmt.stretchlast
-			else if (spf < 1 - cfmt.maxshrink)
-				spf = 1 - cfmt.maxshrink
+			else if (!ll)
+				spf = 1 - cfmt.maxshrink * .75
 			if (spf > (width - xs) / xx)
 				spf = (width - xs) / xx
 		}
